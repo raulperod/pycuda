1e2d6cfd9a4a82ec13c89bc105227eae5192163c
Raul Perod <raulperod@gmail.com>
2018-03-01 21:55:06 -0700

    escala de grises

--- Tarea3/grey.py
+++ Tarea3/grey.py
@@ -4,24 +4,26 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 from scipy.misc import imread, imsave
 
-TPB = 32
+TPB = 16
 
 mod = SourceModule("""
+    #include <stdio.h>
     #define CHANNELS 3
     
-    __global__ void greyscale(int* image, int width, int height) {
+    __global__ void greyscale(float *image, int *dims) {
         int i = threadIdx.x + blockIdx.x * blockDim.x;
         int j = threadIdx.y + blockIdx.y * blockDim.y;
         
-        if (i < width && j < height) {
-            int index = j + i*width;
-            int index3 = index*CHANNELS;
-            value = 0.21*image[index3] + 0.71*image[index3+1] + 0.07*image[index3+2];
+        if (i < dims[0] && j < dims[1]) {
+            int index = j + i*dims[0]; 
+            int index3 = index*CHANNELS;   
+            float value = 0.21*image[index3] + 0.71*image[index3+1] + 0.07*image[index3+2];
             image[index3] = value;
             image[index3+1] = value;
             image[index3+2] = value;
         }
     }
+
 """)
 
 def product(vector):
@@ -40,32 +42,34 @@ def grayscale(image):
         
     return vector
 
-def serial(input_name='test.jpg', output_name='test_out.jpg'):
+def serial(input_name='test_2.jpg', output_name='test_out.jpg'):
     image = imread(input_name)
     # convert image to grayscale
     image_greyscale = grayscale(image).reshape(image.shape)
     # save image
     imsave(output_name, image_greyscale)
 
-def parallel():
-    image = imread(input_name)
+def parallel(input_name="test_2.jpg", output_name="test_out.jpg"):
+    # get kernel
+    greyscale_kernel = mod.get_function("greyscale")    
+    # get image    
+    image = imread(input_name).astype(np.float32)
+    # get the dimensions of the image
+    width, height, channels = image.shape
+    dims = np.array([width, height]).astype(np.int32)
     # change dimension to linear
     image = image.reshape(product(image.shape))
-    # get the dimensions of the image
-    width, height, channels = image.shape 
-    # get kernel
-    greyscale_kernel = mod.get_function("greyscale")
     # convert image to grayscale
-    grayscale_kernel(cuda.Out(image), cuda.In(width), cuda.In(height), 
-        block=(TPB, TPB, 1), grid=((width+1)//TPB, (height+1)//TPB, 1) )
+    greyscale_kernel(cuda.InOut(image), cuda.In(dims),
+        block=(TPB, TPB, 1), grid=( int((width+TPB)//TPB), int((height+TPB)//TPB), 1) )
     # save image
     imsave(output_name, image.reshape(width, height, channels))
 
-def execute(method='serial'):
+def execute(method='parallel'):
     if method is 'serial':
         serial()
     else:
-        parallel():
+        parallel()
 
 if __name__ == '__main__':
     execute()
